e7957c3f18bf6cc54e074b620f64f0fd38adad85
arsl <arsl@optimageeks.com>
2017-04-11 10:19:52 +0500

    new updates and fix added percentage

--- js/app.js
+++ js/app.js
@@ -6,45 +6,21 @@ var app = angular.module('myApp', ['ngRoute', 'angularFileUpload', 'thumbs', 'fb
 /**
  * Configure the Routes
  */
-'use strict';
-app.config(function ($stateProvider, $urlRouterProvider, $compileProvider) {
-    $urlRouterProvider.otherwise('/home');
-    $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'views/partials/home.html',
-                controller: 'siteController'
-            })
-            .state('login', {
-                url: '/login',
-                templateUrl: 'views/partials/login.html',
-                controller: 'loginController'
-            })
-            .state('about', {
-                url: '/about',
-                templateUrl: 'views/partials/about.html',
-                controller: 'siteController'
-            })
-            .state('contact', {
-                url: '/contact',
-                templateUrl: 'views/partials/contact.html',
-                controller: 'siteController'
-            })
-            .state('pricing', {
-                url: '/pricing',
-                templateUrl: 'views/partials/contact.html',
-                controller: 'siteController'
-            })
-});
 app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
         $routeProvider
-
+                .when("/", {templateUrl: "views/partials/home.html", controller: "siteController"})
+                .when("/login", {templateUrl: "views/partials/login.html", controller: "loginController"})
+//                .when("/logout", {controller: "siteController"})
+                .when("/about", {templateUrl: "views/partials/about.html", controller: "siteController"})
+                .when("/contact", {templateUrl: "views/partials/contact.html", controller: "siteController"})
+                .when("/pricing", {templateUrl: "views/partials/pricing.html", controller: "siteController"})
                 .when("/services", {templateUrl: "views/partials/services.html", controller: "siteController"})
                 .when("/work", {templateUrl: "views/partials/work.html", controller: "siteController"})
                 .when("/demo", {templateUrl: "views/partials/demo.html", controller: "demoController"})
                 .when("/slider", {templateUrl: "views/partials/slider.html", controller: "siteController"})
                 .when("/uploads", {templateUrl: "views/partials/uploads.html", controller: "uploadsController"})
                 .when("/listing", {templateUrl: "views/partials/listing.html", controller: "listingController"})
+                .when("/photoDetails/:id", {templateUrl: "views/partials/photoColor.html", controller: "detailController"})
                 .otherwise("/404", {templateUrl: "views/partials/404.html", controller: "siteController"});
         $httpProvider.interceptors.push('authInterceptor');
     }]);
